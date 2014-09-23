c3d2025a23fb965b374772214238e35d5a03c25e
Desola <contact@adriendesola.com>
2014-09-23 14:19:25 +0200

    movePlayer in work

--- js/app.js
+++ js/app.js
@@ -1,46 +1,44 @@
 function movePlayer(){
 	nouvellePos = joueur.Position.eta();
 	var posYActuel = $("#player").css('top');
-	console.log('posYActuel : '+posYActuel);
+	console.log('posYActuel : '+posYActuel+', nouvellePos'+nouvellePos['x']);
 	var duree = 1000;
 
-	$("#player").velocity({ top: [posYActuel,nouvellePos['x']],  rotateZ: [0,-90] },{duration:duree/2,
+	$("#player").velocity({ top: nouvellePos['x'],  rotateZ: [0,-90] },{duration:duree/2,
 		complete:function(){
-			$("#player").velocity({top:[nouvellePos['x'],posYActuel], rotateZ: [0,-90]},{duration:duree/2,
-			complete:function(){
 				joueur._allowJump=true;
 			}
 		 });
-		}
-	});
 }
 
 $(document).ready(function() {
 	niveau0 = $(".sol").css("top");
 	niveau0 = parseInt(niveau0,10) - 2;
 
+	redline = Object.create(sol);
+	
 	joueur = Object.create(player);
+	joueurTaille = joueur.Taille.eta();	
+	
+	redline.initDefault();
 
-	joueurTaille = joueur.Taille.eta();
-	joueur.init(niveau0-joueurTaille['h'],50);
+	joueur.init(350,50);
 
 	joueurPos = joueur.Position.eta();
-
-	redline = Object.create(sol);
-	redline.initDefault;
 
 	// on ajoute le joueur au monde 
 	$(".world").append('<div id="player" class="player"></div>');
 	//on place le joueur dans le monde (en css)
 	$("#player").css('top',joueurPos['x']).css('left',joueurPos['y']);
 	$("#player").css('width',joueurTaille['w']).css('height',joueurTaille['h']);
+
+	joueur.collision(redline);
 });
 
 	$(document).mousedown(function(){
 		if(joueur._allowJump){
 			joueur.jump();
 			movePlayer();
-			console.log(joueur.Position.eta());
 		}else{
 			console.log('movePlayer : FALSE');
 			return false;
